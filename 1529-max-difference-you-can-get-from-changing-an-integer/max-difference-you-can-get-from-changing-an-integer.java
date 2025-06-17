18a9ee1f2e33b634e495cd4681452bb0c779fcb7
Sandeepcmd <sc858889@gmail.com>
2025-06-17 13:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (77.05%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
@@ -7,14 +7,11 @@ class Solution {
             n1 /= 10;
         }
 
-        int c = c1;
-        int n2 = num;
-        int pow = 1;
-        for (int i = 1; i < c; i++) pow *= 10;
-
         int digit = -1;
-        for (int p = pow; p > 0; p /= 10) {
-            int a = (n2 / p) % 10;
+        for (int pos = c1; pos > 0; pos--) {
+            int div = 1;
+            for (int j = 1; j < pos; j++) div *= 10;
+            int a = (num / div) % 10;
             if (a != 9) {
                 digit = a;
                 break;
@@ -22,39 +19,51 @@ class Solution {
         }
 
         int max = 0;
-        n2 = num;
-        for (int p = pow; p > 0; p /= 10) {
-            int a = (n2 / p) % 10;
+        int n2 = num;
+        for (int pos = c1; pos > 0; pos--) {
+            int div = 1;
+            for (int j = 1; j < pos; j++) div *= 10;
+            int a = n2 / div;
+            n2 %= div;
             if (a == digit) a = 9;
             max = max * 10 + a;
         }
 
-        n2 = num;
-        int first = n2 / pow;
-        int replaceDigit = -1;
-        int replaceWith = 1;
+        int min = minimum(c1, num);
+        return max - min;
+    }
 
+    private int minimum(int c, int num) {
+        int div = 1;
+        for (int j = 1; j < c; j++) div *= 10;
+        int first = num / div;
+
+        int changeDigit;
+        int changeTo;
         if (first != 1) {
-            replaceDigit = first;
-            replaceWith = 1;
+            changeDigit = first;
+            changeTo = 1;
         } else {
-            for (int p = pow / 10; p > 0; p /= 10) {
-                int a = (n2 / p) % 10;
-                if (a != 0 && a != 1) {
-                    replaceDigit = a;
-                    replaceWith = 0;
-                    break;
-                }
+            changeDigit = -1;
+            changeTo = 0;
+            for (int pos = c - 1; pos > 0 && changeDigit == -1; pos--) {
+                int d = 1;
+                for (int k = 1; k < pos; k++) d *= 10;
+                int a = (num / d) % 10;
+                if (a != 0 && a != 1) changeDigit = a;
             }
         }
 
+        int n2 = num;
         int min = 0;
-        for (int p = pow; p > 0; p /= 10) {
-            int a = (n2 / p) % 10;
-            if (a == replaceDigit) a = replaceWith;
+        for (int pos = c; pos > 0; pos--) {
+            int d = 1;
+            for (int k = 1; k < pos; k++) d *= 10;
+            int a = n2 / d;
+            n2 %= d;
+            if (a == changeDigit) a = changeTo;
             min = min * 10 + a;
         }
-
-        return max - min;
+        return min;
     }
 }
