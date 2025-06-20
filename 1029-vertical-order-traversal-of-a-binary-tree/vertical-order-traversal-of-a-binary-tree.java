1146fb5c02b58b958eb42c208c9d57086bee0bf7
Sandeepcmd <sc858889@gmail.com>
2025-06-20 14:29:06 +0530

    Time: 3 ms (81.64%) | Memory: 42.5 MB (66.26%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -1,50 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     class Info {
         TreeNode node;
         int level;
         int hd;
-
-        Info(TreeNode node, int level, int hd) {
+        Info(TreeNode node,int level,int hd)
+        {
             this.node = node;
             this.level = level;
             this.hd = hd;
         }
     }
+    class Pair{
+        int first;
+        int level;
+        Pair(int first,int level)
+        {
+            this.first = first;
+            this.level = level;
+        }
+    }
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        PriorityQueue<Info> pq1 = new PriorityQueue<>(
-            (a, b) -> a.level - b.level 
-        );
-        pq1.offer(new Info(root, 0, 0));
-
-        PriorityQueue<Info> pq2 = new PriorityQueue<>((a, b) -> {
-            if (a.hd != b.hd) return a.hd - b.hd;
-            if (a.level != b.level) return a.level - b.level;
-            return a.node.val - b.node.val;
-        });
-
-        while (!pq1.isEmpty()) {
-            Info curr = pq1.poll();
-            pq2.offer(curr);
-
+        HashMap<Integer, List<Pair>> map = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        Queue<Info> que = new LinkedList<>();
+        que.offer(new Info(root, 0, 0));
+        while (que.size() > 0) {
+            Info curr = que.remove();
             TreeNode node = curr.node;
-            int level = curr.level;
             int hd = curr.hd;
-
+            int level = curr.level;
+            min = Math.min(min,hd);
+            max = Math.max(max,hd);
+            map.computeIfAbsent(hd, k -> new ArrayList<>());
+            map.get(hd).add(new Pair(node.val, level));
             if (node.left != null) {
-                pq1.offer(new Info(node.left, level + 1, hd - 1));
+                que.offer(new Info(node.left, level + 1, hd - 1));
             }
             if (node.right != null) {
-                pq1.offer(new Info(node.right, level + 1, hd + 1));
+                que.offer(new Info(node.right, level + 1, hd + 1));
             }
         }
-
-        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
-        while (!pq2.isEmpty()) {
-            Info info = pq2.poll();
-            map.computeIfAbsent(info.hd, k -> new ArrayList<>()).add(info.node.val);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=min;i<=max;i++)
+        {
+            List<Pair> temp = new ArrayList<>();
+            temp = map.get(i);
+            Collections.sort(temp,(p1,p2)->{
+                  if(p1.level == p2.level)
+                  {
+                    return p1.first-p2.first;
+                  }
+                  else
+                  {
+                    return p1.level-p2.level;
+                  }
+            });
+            List<Integer> temp2 = new ArrayList<>();
+            for(Pair p : temp)
+            {
+                temp2.add(p.first);
+            } 
+            ans.add(temp2);
         }
-
-        return new ArrayList<>(map.values());
+        return ans;
     }
 }
