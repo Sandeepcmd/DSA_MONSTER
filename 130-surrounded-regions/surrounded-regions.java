91a20990267bcc3a32af7bd112d4ca57fe9fbffc
Sandeepcmd <sc858889@gmail.com>
2025-06-24 13:58:23 +0530

    Time: 3 ms (32.56%) | Memory: 45.2 MB (92.59%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,47 +1,42 @@
 class Solution {
-    public void solve(char[][] board) 
-    {
-        int n=board.length;
-        int m=board[0].length;
-        boolean visit[][]=new boolean[n][m];
-        Queue<int []> que=new LinkedList<>();
-        for(int i=0;i<n;i++)
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        boolean visit[][] = new boolean[n][m]; 
+        Queue<int []> que = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((i == 0 || j == 0 || i == n-1 || j == m-1 )&&(board[i][j] == 'O') ){
+                    que.offer(new int[]{i,j});
+                    visit[i][j] = true;
+                }
+            }
+        }
+        int drow[] = {1,0,-1,0};
+        int dcol[] = {0,1,0,-1};
+        while(que.size() > 0)
         {
+             int curr[] = que.remove();
+             int i = curr[0];
+             int j = curr[1];
+             for(int k=0;k<4;k++){
+                int nrow = i+drow[k];
+                int ncol = j+dcol[k];
+                if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && board[nrow][ncol] == 'O' && !visit[nrow][ncol])
+                {
+                    visit[nrow][ncol] = true;
+                    que.offer(new int[]{nrow,ncol});
+                }
+             }
+        }
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++)
             {
-                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
-                    if(board[i][j] == 'O') {
-                        que.add(new int[]{i,j}); 
-                        visit[i][j] = true; 
-                    }
+                if(board[i][j] == 'O' && !visit[i][j]){
+                      board[i][j] = 'X';
                 }
             }
         }
-        int drow[]={1,0,-1,0};
-        int dcol[]={0,-1,0,1};
-        while(!que.isEmpty())
-        {
-            int curr[]=que.remove();
-            for(int i=0;i<4;i++)
-            {
-                int x=curr[0]+drow[i];
-                int y=curr[1]+dcol[i];
-                if(x>=0 && x<n && y>=0 && y<m && visit[x][y]!=true && board[x][y]=='O')
-                {
-                    que.offer(new int[]{x,y});
-                    visit[x][y]=true;
-                }
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(board[i][j]=='O' && visit[i][j]!=true)
-                {
-                    board[i][j]='X';
-                }
-            }
-        }
+
     }
 }
