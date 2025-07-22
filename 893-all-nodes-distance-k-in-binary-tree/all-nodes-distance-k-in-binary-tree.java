64d87fe7cbb21ee9c35e5c5c2692be379aef1506
SANDEEP <sc858889@gmail.com>
2025-07-22 12:11:06 +0530

    Time: 12 ms (89.36%) | Memory: 43.2 MB (16.29%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,73 +8,54 @@
  * }
  */
 class Solution {
-    class Pair
-    {
+    class Pair{
         TreeNode first;
         int second;
-        Pair(TreeNode first,int second)
-        {
+        Pair(TreeNode first,int second){
             this.first = first;
             this.second = second;
         }
     }
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         HashMap<TreeNode,TreeNode> map = new HashMap<>();
+        bfs(root,map);
+        boolean visited[] = new boolean[500+1];
         List<Integer> list = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while(queue.size() > 0)
-        {
-            int n = queue.size();
-            while(n-->0)
-            {
-                TreeNode curr = queue.remove();
-                if(curr.left != null)
-                {
-                    map.put(curr.left,curr);
-                    queue.offer(curr.left);
-                }
-                 if(curr.right != null)
-                {
-                    map.put(curr.right,curr);
-                    queue.offer(curr.right);
-                }
-            }
-        }
-        Set<TreeNode> set = new HashSet<>();
-        set.add(target);
+        if(target == null)return list;
+       
         Queue<Pair> que = new LinkedList<>();
         que.offer(new Pair(target,0));
-         while(que.size() > 0)
+        visited[target.val] = true;
+        while(que.size() > 0)
         {
-            int n = que.size();
-            while(n-->0)
-            {
-                Pair p = que.remove();
-                TreeNode curr = p.first;
-                int dist = p.second;
-                if(dist == k)
-                {
-                    list.add(curr.val);
-                }
-                if(curr.left != null && !set.contains(curr.left))
-                {
-                    set.add(curr.left);
-                    que.offer(new Pair(curr.left,dist+1));
-                }
-                if(curr.right != null && !set.contains(curr.right))
-                {
-                    set.add(curr.right);
-                    que.offer(new Pair(curr.right,dist+1));
-                }
-                if(map.containsKey(curr) && !set.contains(map.get(curr)))
-                {
-                      set.add(map.get(curr));
-                      que.offer(new Pair(map.get(curr),dist+1));
-                }
-            }
-        }
+            Pair curr = que.poll();
+            TreeNode node = curr.first;
+            visited[node.val] = true;
+            int dis = curr.second;
+            if(dis == k)list.add(node.val);
+            if(node.left != null && !visited[node.left.val])que.offer(new Pair(node.left,dis+1));
+            if(node.right != null && !visited[node.right.val])que.offer(new Pair(node.right,dis+1));
+            if(map.get(node) != null && !visited[map.get(node).val])que.offer(new Pair(map.get(node),dis+1));
+            
+        }        
         return list;
     }
-
+    public void bfs(TreeNode root, HashMap<TreeNode,TreeNode> map)
+    {
+        if(root == null)return;
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+        while(que.size() > 0)
+        {
+            TreeNode curr = que.remove();
+            if(curr.left != null){
+                map.put(curr.left,curr);
+                que.offer(curr.left);
+            }
+            if(curr.right != null){
+                map.put(curr.right,curr);
+                que.offer(curr.right);
+            }
+        }
+    }
 }
