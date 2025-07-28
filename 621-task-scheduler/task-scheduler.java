1b9cf5ad6ee48c7f9574447bbedfd706aef7021a
SANDEEP <sc858889@gmail.com>
2025-07-28 08:51:50 +0530

    Time: 2440 ms (5.02%) | Memory: 46.6 MB (5.20%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,38 +1,44 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-         int ans = 0;
-         int map[] = new int[26];
-         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
-         for(int i=0;i<tasks.length;i++)
-         {
+        String ans = "";
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        int len = tasks.length;
+        int map[] = new int[26];
+        for(int i=0;i<len;i++)
+        {
             map[tasks[i]-'A']++;
-         }
-         for(int i=0;i<26;i++)
-         {
+        }
+        for(int i=0;i<26;i++)
+        {
             if(map[i] > 0)pq.offer(map[i]);
-         }
-         while(pq.size() > 0)
-         {
-            List<Integer> list= new ArrayList<>();
-            for(int i=1;i<=n+1;i++)
-            {
-                if(pq.size()>0)
-                {
-                    int freq = pq.poll();
-                    freq--;
-                    list.add(freq);
+        }
+       
+        while(pq.size() > 0)
+        {
+             List<Integer> temp = new ArrayList<>();
+             for(int i=0;i<n+1;i++)
+             {
+                if(pq.size() == 0 )ans += "#";
+                else{
+                   int curr = pq.remove()-1;
+                   if(curr != 0){temp.add(curr);}
+                   ans += "1";
                 }
+             }
+             for(int ele : temp){
+                System.out.println(ele);
+                pq.offer(ele);
+             }
+
+        }
+        int leng = 0;
+        for(int i=ans.length()-1;i>=0;i--)
+        {
+            if(ans.charAt(i) == '#')leng++;
+            else{
+                break;
             }
-            for(int i=0;i<list.size();i++)
-            {
-                if(list.get(i)>0)pq.offer(list.get(i));
-            }
-            if(pq.size() == 0)
-            {
-              ans += list.size();
-            }
-            else ans += n+1;
-         }
-         return ans;
+        }
+        return ans.length()-leng;
     }
 }
