6c715e9168756a57ce1a913730361ffb83070f51
SANDEEP <sc858889@gmail.com>
2025-08-09 15:19:55 +0530

    Time: 2 ms (82.95%) | Memory: 44.8 MB (30.68%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        List<List<Integer>>list = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        help(0,list,temp,arr,target);
+        return list;
+    }
+    public void help(int idx,List<List<Integer>>list,List<Integer> temp,int arr[],int target)
+    {
+        if(target < 0)return;
+        if(target == 0)
+        {
+            list.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=idx;i<arr.length;i++)
+        {
+            temp.add(arr[i]);
+            help(i,list,temp,arr,target-arr[i]);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
