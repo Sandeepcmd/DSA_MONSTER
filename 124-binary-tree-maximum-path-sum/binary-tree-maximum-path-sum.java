e9368984d6cb7c12bed654762d60d41fc3afa44e
SANDEEP <sc858889@gmail.com>
2025-08-28 12:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (18.01%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -16,16 +16,19 @@
 class Solution {
     public int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        int not = path(root);
-        return max;
+       int ans = maxPathSum1(root);
+       return max; 
     }
-    public int path(TreeNode root)
+    public int maxPathSum1(TreeNode root)
     {
-        if(root == null)return 0;
-        int left = Math.max(0,path(root.left));
-        int right = Math.max(0,path(root.right));
-        int sum = root.val+left+right;
-        max = Math.max(max,sum);
+         if(root == null)
+        {
+            return 0;
+        }
+        int left = Math.max(0,maxPathSum1(root.left));
+        int right = Math.max(0,maxPathSum1(root.right));
+        int self = root.val + left + right;
+        max = Math.max(max,self);
         return Math.max(left,right)+root.val;
-     }
+    }
 }
