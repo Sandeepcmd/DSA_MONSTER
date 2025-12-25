f392762708e6b3206a0a62dd8e244c5372637d8a
SANDEEP <sc858889@gmail.com>
2025-12-25 09:21:11 +0530

    Time: 121 ms (9.14%) | Memory: 122.5 MB (5.38%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long maximumHappinessSum(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        for(int i=0;i<arr.length;i++)
+        {
+            pq.offer(arr[i]);
+        }
+        int i = 0;
+        long ans = 0;
+        while(pq.size() > 0 && i < k)
+        {
+            int ele = pq.remove()-i;
+            int add = (ele < 0)?0:ele;
+            ans = ans + (long)add;
+            i++;
+        }
+        return ans;
+    }
+}
