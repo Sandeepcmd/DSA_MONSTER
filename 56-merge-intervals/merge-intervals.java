63f94d809b9e262e2869e79f6620359b9f271f5c
SANDEEP <sc858889@gmail.com>
2026-01-30 08:28:54 +0530

    Time: 11 ms (17.82%) | Memory: 48.8 MB (87.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,29 +1,23 @@
-import java.util.*;
-
 class Solution {
-    public int[][] merge(int[][] intervals) {
-       
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-
-        List<int[]> result = new ArrayList<>();
-        int n = intervals.length;
-        int pf = intervals[0][0];
-        int ps = intervals[0][1];
-
-        for (int i = 1; i < n; i++) {
-            int cf = intervals[i][0];
-            int cs = intervals[i][1];
-
-            if (cf <= ps) {
-                ps = Math.max(ps, cs);
-            } else {
-                result.add(new int[]{pf, ps});
-                pf = cf;
-                ps = cs;
-            }
+    public int[][] merge(int[][] arr) {
+        ArrayList<int []> list = new ArrayList<>();
+        Arrays.sort(arr,(a,b)->a[0]-b[0]);
+        int n = arr.length;
+        int x = arr[0][0];
+        int y = arr[0][1];
+        for(int i=1;i<n;i++){
+             if(arr[i][0] <=  y){
+                y = Math.max(y,arr[i][1]);
+             }
+             else{
+                list.add(new int[]{x,y});
+                 x = arr[i][0];
+                 y = arr[i][1];
+             }
+             
         }
-        result.add(new int[]{pf, ps});
+        list.add(new int[]{x,y});
+        return list.toArray(new int[list.size()][]);
 
-        return result.toArray(new int[result.size()][]);
     }
 }
